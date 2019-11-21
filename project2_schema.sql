2d1b15201e00c91400a260647080da2d7f59eb32
Zachary Pillman <zachary.pillman@gmail.com>
2019-11-21 14:35:06 -0600

    Updated Schema to have the same names as the provided schema

--- project2_schema.sql
+++ project2_schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE BorrowersFile
 (
     borrower_id INT,
     ssn VARCHAR(11),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
+    Bname VARCHAR(255),
+    Bname_last VARCHAR(255),
     email VARCHAR(255),
     addr VARCHAR(255),
     city VARCHAR(255),
@@ -17,7 +17,7 @@ CREATE TABLE BorrowersFile
 
 CREATE TABLE BooksFile
 (
-    isbn10 VARCHAR(10),
+    isbn VARCHAR(10),
     isbn13 VARCHAR(13),
     title VARCHAR(255),
     author VARCHAR(255),
@@ -34,7 +34,7 @@ COPY BooksFile FROM '/Users/zpillman/School/CS_4347_Database_Systems/project_2/b
 CREATE TABLE Authors
 (
  author_id serial NOT NULL,
- full_name varchar(255) NOT NULL
+ name text[] NOT NULL
 );
 
 CREATE UNIQUE INDEX PK_Authors ON Authors
@@ -43,32 +43,32 @@ CREATE UNIQUE INDEX PK_Authors ON Authors
 );
 
 --BookAuthors Table
-CREATE TABLE BookAuthors
+CREATE TABLE Book_Authors
 (
  author_id serial NOT NULL,
- isbn10    varchar(10) NOT NULL
+ isbn    varchar(10) NOT NULL
 );
 
-CREATE UNIQUE INDEX PK_BookAuthors ON BookAuthors
+CREATE UNIQUE INDEX PK_Book_Authors ON Book_Authors
 (
  author_id,
- isbn10
+ isbn
 );
 
-CREATE INDEX fkIdx_117 ON BookAuthors
+CREATE INDEX fkIdx_117 ON Book_Authors
 (
- isbn10
+ isbn
 );
 
-CREATE INDEX fkIdx_63 ON BookAuthors
+CREATE INDEX fkIdx_63 ON Book_Authors
 (
  author_id
 );
 
 --Books Table
-CREATE TABLE Books
+CREATE TABLE Book
 (
- isbn10    varchar(10) NOT NULL,
+ isbn    varchar(10) NOT NULL,
  title     varchar(255) NOT NULL,
  cover     varchar(255) NULL,
  publisher varchar(255) NULL,
@@ -76,40 +76,40 @@ CREATE TABLE Books
  is_checked_out boolean DEFAULT false
 );
 
-CREATE UNIQUE INDEX PK_book ON Books
+CREATE UNIQUE INDEX PK_book ON Book
 (
- isbn10
+ isbn
 );
 
 --BookLoans
-CREATE TABLE BookLoans
+CREATE TABLE Book_Loans
 (
  date_out date NOT NULL DEFAULT CURRENT_DATE,
  due_date date NOT NULL DEFAULT CURRENT_DATE + 14,
  date_in  date NULL,
  card_id  serial NOT NULL,
  loan_id  serial NOT NULL,
- isbn10   varchar(10) NOT NULL
+ isbn   varchar(10) NOT NULL
 );
 
-CREATE UNIQUE INDEX PK_BookLoans ON BookLoans
+CREATE UNIQUE INDEX PK_Book_Loans ON Book_Loans
 (
  loan_id
 );
 
-CREATE INDEX fkIdx_111 ON BookLoans
+CREATE INDEX fkIdx_111 ON Book_Loans
 (
  card_id
 );
 
-CREATE INDEX fkIdx_114 ON BookLoans
+CREATE INDEX fkIdx_114 ON Book_Loans
 (
  loan_id
 );
 
-CREATE INDEX fkIdx_123 ON BookLoans
+CREATE INDEX fkIdx_123 ON Book_Loans
 (
- isbn10
+ isbn
 );
 
 --Fines Table
@@ -126,12 +126,12 @@ CREATE UNIQUE INDEX PK_Fines ON Fines
 );
 
 --Borrowers Table
-CREATE TABLE Borrowers
+CREATE TABLE Borrower
 (
  card_id    serial NOT NULL,
  ssn        varchar(11) NOT NULL,
- first_name varchar(255) NOT NULL,
- last_name  varchar(255) NOT NULL,
+ Bname      varchar(255) NOT NULL,
+ Bname_last  varchar(255) NOT NULL,
  email      varchar(255) NULL,
  address    varchar(255) NOT NULL,
  city       varchar(255) NOT NULL,
@@ -139,7 +139,7 @@ CREATE TABLE Borrowers
  phone      varchar(16) NOT NULL
 );
 
-CREATE UNIQUE INDEX PK_Borrowers ON Borrowers
+CREATE UNIQUE INDEX PK_Borrower ON Borrower
 (
  card_id
 );
@@ -148,40 +148,41 @@ CREATE UNIQUE INDEX PK_Borrowers ON Borrowers
 CREATE TABLE Isbns
 (
  isbn13 varchar(13) NULL,
- isbn10  varchar(10) NOT NULL
+ isbn  varchar(10) NOT NULL
 );
 
 CREATE INDEX fkIdx_120 ON Isbns
 (
- isbn10
+ isbn
 );
 
 --Execute queries to move data into expected places
-INSERT INTO Borrowers(ssn, first_name, last_name, email, address, city, state, phone)
-SELECT ssn, first_name, last_name, email, addr, city, state, phone FROM BorrowersFile;
+INSERT INTO Borrower(ssn, Bname, Bname_last, email, address, city, state, phone)
+SELECT ssn, Bname, Bname_last, email, addr, city, state, phone FROM BorrowersFile;
 
-INSERT INTO Books(isbn10, title, cover, publisher, pages)
-SELECT isbn10, title, cover, publisher, pages FROM BooksFile;
+INSERT INTO Book(isbn, title, cover, publisher, pages)
+SELECT isbn, title, cover, publisher, pages FROM BooksFile;
 
-INSERT INTO Isbns(isbn10, isbn13)
-SELECT isbn10, isbn13 FROM BooksFile;
+INSERT INTO Isbns(isbn, isbn13)
+SELECT isbn, isbn13 FROM BooksFile;
 
 --Don't accept books with no author
-INSERT INTO Authors(full_name)
-SELECT author FROM BooksFile
-WHERE author IS NOT NULL;
+--split the comma delimited String of authors into an array
+INSERT INTO Authors(name)
+SELECT DISTINCT regexp_split_to_array(BooksFile.author, '\,') FROM BooksFile
+WHERE BooksFile.author IS NOT NULL;
 
-INSERT INTO BookAuthors(author_id, isbn10)
-SELECT Authors.author_id, BooksFile.isbn10 FROM Authors
-JOIN BooksFile ON BooksFile.author = Authors.full_name;
+INSERT INTO Book_Authors(author_id, isbn)
+SELECT DISTINCT Authors.author_id, BooksFile.isbn FROM Authors
+JOIN BooksFile ON regexp_split_to_array(BooksFile.author, '\,') = Authors.name;
 
 --Add constraints to tables
-ALTER TABLE BookAuthors ADD CONSTRAINT FK_117 FOREIGN KEY ( isbn10 ) REFERENCES Books ( isbn10 );
-ALTER TABLE BookAuthors ADD CONSTRAINT FK_63 FOREIGN KEY ( author_id ) REFERENCES Authors ( author_id );
-ALTER TABLE Isbns ADD CONSTRAINT FK_120 FOREIGN KEY ( isbn10 ) REFERENCES Books ( isbn10 );
-ALTER TABLE BookLoans ADD CONSTRAINT FK_111 FOREIGN KEY ( card_id ) REFERENCES Borrowers ( card_id );
-ALTER TABLE Fines ADD CONSTRAINT FK_114 FOREIGN KEY ( loan_id ) REFERENCES BookLoans ( loan_id );
-ALTER TABLE BookLoans ADD CONSTRAINT FK_123 FOREIGN KEY ( isbn10 ) REFERENCES Books ( isbn10 );
+ALTER TABLE Book_Authors ADD CONSTRAINT FK_117 FOREIGN KEY ( isbn ) REFERENCES Book ( isbn );
+ALTER TABLE Book_Authors ADD CONSTRAINT FK_63 FOREIGN KEY ( author_id ) REFERENCES Authors ( author_id );
+ALTER TABLE Isbns ADD CONSTRAINT FK_120 FOREIGN KEY ( isbn ) REFERENCES Book ( isbn );
+ALTER TABLE Book_Loans ADD CONSTRAINT FK_111 FOREIGN KEY ( card_id ) REFERENCES Borrower ( card_id );
+ALTER TABLE Fines ADD CONSTRAINT FK_114 FOREIGN KEY ( loan_id ) REFERENCES Book_Loans ( loan_id );
+ALTER TABLE Book_Loans ADD CONSTRAINT FK_123 FOREIGN KEY ( isbn ) REFERENCES Book ( isbn );
 
 --Drop Temp Tables
 DROP TABLE BorrowersFile;
